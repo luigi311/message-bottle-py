d0991b25f2d6d4246563f5ea8032b4a77bc3f8e5
AlfredoMondragon <amondragon4@live.nmhu.edu>
2017-04-13 11:53:03 -0600

    Update urls.py

--- message_bottle/urls.py
+++ message_bottle/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^thread/add/$', ThreadCreateView.as_view(), name='thread_add'),
     url(r'^admin/', admin.site.urls),
+    url(r'^thread', DetailThreadView.as_view(), name='thread_view'),
     
     url(r'^thread/edit/(?P<pk>\d+)/$', UpdateThreadView.as_view(), name = 'thread_update')
 ]
